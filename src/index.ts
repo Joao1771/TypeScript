617e34401787768156780c5655883f0c39e70008
João Flávio <joaoflaviog1@gmail.com>
2025-07-31 16:00:36 -0300

    TS: instalação 261 - 269

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+// function ola(oq) {
+//     return `olá ${oq}`;
+// }
+
+// console.log(ola("mundo!"));
+
+// const nome = "joao"
+
+// export default nome;
+
+//   SCRIPT MODE:Tudo em um script único sem exportar e importar.
+
+// MODULE MODE:Modularizar (separar) o código em mais de um arquivo
+// para ativar deve importar ou exportar algo. 99,9% dos casos.
+
+console.log("nice")
