fe529924593358fafe7a835efe1603620381cdbd
João Flávio <168208141+Joao1771@users.noreply.github.com>
2025-08-28 12:52:51 -0300

    webpack part I II

---
+++ webpack.config.cjs
@@ -0,0 +1,30 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/a018-webpack/index.ts',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader',
+        exclude: /node_modules/,
+        options: {
+          configFile: 'tsconfig-frontend.json'
+        }
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+      extensionAlias: {
+    ".js": [".ts", ".js"],  // se pedirem .js, pode resolver .ts também
+    ".jsx": [".tsx", ".jsx"],
+  },
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'frontend','assets','js'),
+  },
+  devtool: 'source-map',
+};
